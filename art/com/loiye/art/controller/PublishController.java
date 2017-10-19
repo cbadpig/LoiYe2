89d7fc7c5f54935e2a0faf9129c456b42c7b941e
cbadpig <891511545@qq.com>
2017-10-19 18:02:09 +0800

    2017-10-19

--- art/com/loiye/art/controller/PublishController.java
+++ art/com/loiye/art/controller/PublishController.java
@@ -3,15 +3,18 @@ package com.loiye.art.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/dog")
 public class PublishController {
 
     @GetMapping("")
-    public ModelAndView accessArtHome() {
-        return new ModelAndView("/templates/index.html");
+    public void accessArtHome(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("templates/index.html");
     }
 
 }
