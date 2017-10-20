466299cd6d50249a1b4181eb7fada6fa6eb3e041
cbadpig <891511545@qq.com>
2017-10-20 13:43:15 +0800

    2017-10-20

--- art/com/loiye/art/controller/ArtController.java
+++ art/com/loiye/art/controller/ArtController.java
@@ -6,12 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/dog/note")
+@RequestMapping("/huaishu")
 public class ArtController {
 
     @GetMapping("")
     public ModelAndView accessArtHome() {
+        return  new ModelAndView("templates/art/index.html");
+    }
+
+    @GetMapping("/note")
+    public ModelAndView accessArtHuaiShu() {
         return  new ModelAndView("templates/art/note/note.html");
     }
+
 
 }
--- art/com/loiye/art/controller/NoteController.java
+++ art/com/loiye/art/controller/NoteController.java
@@ -1,17 +1,11 @@
 package com.loiye.art.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/dog")
+@RequestMapping("/yangshu")
 public class NoteController {
 
-    @GetMapping("")
-    public ModelAndView accessArtHome() {
-        return  new ModelAndView("templates/art/index.html");
-    }
 
 }
