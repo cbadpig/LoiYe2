b12963ca90e64c4d08d1517bef08919f350c51c9
cbadpig <891511545@qq.com>
2017-10-30 23:18:05 +0800

    2017-10-30

--- web/resource/UEditor-1.4.3.3/ueditor.config.js
+++ web/resource/UEditor-1.4.3.3/ueditor.config.js
@@ -34,9 +34,9 @@
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
-            'fullscreen', 'undo', 'redo', '|',
-            'bold', 'italic', 'underline','|','blockquote','horizontal','|',
-            'paragraph','insertimage','|','link', 'unlink','|','insertcode',
+            'fullscreen','insertimage',
+            'bold','paragraph','blockquote','horizontal',
+            '|','link', 'unlink','|','insertcode',
         ]]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
